475fd508ddebf0802d4f502b8a6c495cb3471a70
Allen McCulloch <mccullochallen@gmail.com>
2024-06-06 13:33:52 -0500

    various updates

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/${dbName}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToDatabase() {
@@ -25,3 +25,4 @@ async function connectToDatabase() {
 }
 
 export default connectToDatabase;
+
